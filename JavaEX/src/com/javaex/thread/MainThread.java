ad6c2b87109521f88a790b230974a0f600bb57fe
Jaemin Lim <jmeeen@gmail.com>
2021-08-11 10:37:24 +0900

    synchronized Thread

--- JavaEX/src/com/javaex/thread/MainThread.java
+++ JavaEX/src/com/javaex/thread/MainThread.java
@@ -6,8 +6,7 @@ public class MainThread {
 		// Working thread 호출 --2
 		Thread thread = new DigitThread();
 		thread.setName("DigitThread");
-		// Thread시작 : run메소드 직접 호출하지 말것.
-		thread.start();
+
 
 		// Main Thread 로직 --- 1
 		// A ~ Z 까지 출력
@@ -24,7 +23,13 @@ public class MainThread {
 //		}
 //		
 		Thread thread2 = new Thread(new AlphabetThread());
-
+		
+		// Thread 우선순위 변경
+		// 1 (MIN) ~ 5(Normal) ~ 10(Max)
+		thread.setPriority(Thread.MAX_PRIORITY);
+		thread2.setPriority(Thread.MIN_PRIORITY);
+		// Thread시작 : run메소드 직접 호출하지 말것.
+		thread.start();
 		thread2.start();
 
 		// MainThread의 흐름에 Working Thread들의 흐름을 합류
